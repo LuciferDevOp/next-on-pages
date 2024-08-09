8fba1e25dc870b603cba46e69161797d60c767ca
p1lenovoma40 <p1lenovoma40@gmail.com>
2024-08-09 10:58:28 +0000

    -

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -1,6 +1,6 @@
 
 export default async function page({params}) {
-  const response = await fetch(`https://mybot-md2-default-rtdb.firebaseio.com/${params}`);
-  const data = response.data;
+  // const response = await fetch(`https://mybot-md2-default-rtdb.firebaseio.com/${params}`);
+  const data = {message : "Hello World"};
   return res.status(200).json(data);
 }
