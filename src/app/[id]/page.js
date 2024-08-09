9bf689e181ffe47214ddc4d1b87aa1a9fa20c9b8
p1lenovoma40 <p1lenovoma40@gmail.com>
2024-08-09 11:03:16 +0000

    Initial Commit

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -1,6 +1,8 @@
 
-export default async function page({params}) {
-  // const response = await fetch(`https://mybot-md2-default-rtdb.firebaseio.com/${params}`);
-  const data = {message : "Hello World"};
-  return res.status(200).json(data);
+export default function page({params}) {
+  return (
+    <div>
+      {JSON.stringify(params)}
+    </div>
+  )
 }
